3dc0fbe3042d360a3326f9d7ff5b96b5b7aa74f8
arooshiverma <av22@iitbbs.ac.in>
2020-04-09 12:57:57 +0530

    database

--- DBMS_Project/settings.py
+++ DBMS_Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hospital.apps.HospitalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'DBMS_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospital_schedule',
+        'USER': 'postgres',
+        'PASSWORD': 'av123',
+        'HOST': 'localhost'
     }
 }
 
@@ -122,3 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT =  os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
