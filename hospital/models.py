65aa1405f3cb935e6c88770623185a4ee680158b
arooshiverma <av22@iitbbs.ac.in>
2020-04-14 10:55:39 +0530

    database

--- hospital/models.py
+++ hospital/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Doctor(models.Model):
-    docid = models.IntegerField()
-    docname = models.CharField(max_length=50)
-    deptno = models.IntegerField()
-    deptname = models.CharField(max_length=15)
-    passwd = models.CharField(max_length=50)
-    fees = models.IntegerField()
-    isdoc = models.BooleanField(default=True)
--- hospital/urls.py
+++ hospital/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login', views.login, name="login"),
+    path('book_appoint', views.book_appoint, name="book_appoint"),
 ]
