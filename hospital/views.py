0cbc8bbff7a9cdf2a814322607dd7ab7c431128b
arooshiverma <av22@iitbbs.ac.in>
2020-04-14 10:52:12 +0530

    bleh

--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'Login.html')
+
+def book_appoint(request):
+    return render(request, 'Book_appoint.html')
