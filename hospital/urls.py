aa1edcf3da9ad5ed1e22466c6fedc6f35d7ba488
arooshiverma <av22@iitbbs.ac.in>
2020-04-16 10:32:45 +0530

    login update

--- hospital/urls.py
+++ hospital/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name="index"),
     path('login', views.login, name="login"),
+    path('register', views.register, name="register"),
+    path('register2', views.register2, name="register2"),
     path('book_appoint', views.book_appoint, name="book_appoint"),
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -9,5 +9,14 @@ def index(request):
 def login(request):
     return render(request, 'Login.html')
 
+
+def register(request):
+    return render(request, 'Register1.html')
+
+
+def register2(request):
+    return render(request, 'Register2.html')
+
+
 def book_appoint(request):
     return render(request, 'Book_appoint.html')
